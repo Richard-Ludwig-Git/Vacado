5fdd6e1ed7719f9fe2182aef2cb0c8f797798baf
vladtepesch <vladtepesch@gmx.de>
2025-09-09 18:59:34 +0200

    Titel added

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from backend import security, backend_endpoints
 from frontend import frontend_routes
 
 
-vacado = FastAPI()
+vacado = FastAPI(title="Vacado")
 vacado.include_router(router=security.security_router)
 vacado.include_router(router=frontend_routes.routing)
 vacado.include_router(router=backend_endpoints.backapp)
